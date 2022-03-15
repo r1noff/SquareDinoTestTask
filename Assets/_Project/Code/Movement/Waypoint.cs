f06a09cce44a926d23d674b2f39048d610374470
Ivan Rechkin <rechkinin@gmail.com>
2022-03-15 22:18:04 +0500

    Waypoints Movement

---
+++ Assets/_Project/Code/Movement/Waypoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SquareDino.RechkinTestTask.Movement
+{
+    public class Waypoint : MonoBehaviour
+    {
+        public Vector3 Position => transform.position;
+        
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(Position, 0.5f);
+        }
+#endif
+    }
+}
---
+++ Assets/_Project/Code/Movement/WaypointsMovement.cs
@@ -0,0 +1,15 @@
+﻿using MyBox;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SquareDino.RechkinTestTask.Movement
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class WaypointsMovement : MonoBehaviour
+    {
+        [SerializeField] [AutoProperty] private NavMeshAgent _navMeshAgent;
+        
+        public void MoveTo(Waypoint waypoint) => 
+            _navMeshAgent.SetDestination(waypoint.Position);
+    }
+}
