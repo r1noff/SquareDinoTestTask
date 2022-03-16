222a735121901fff62443c781f6cb7aa5a37da32
Ivan Rechkin <rechkinin@gmail.com>
2022-03-16 07:31:38 +0500

    Ragdoll

---
+++ Assets/_Project/Code/Enemies/EnemyRagdoll.cs
@@ -0,0 +1,28 @@
+using System;
+using MyBox;
+using UnityEngine;
+
+namespace SquareDino.RechkinTestTask.Enemies
+{
+    public class EnemyRagdoll : MonoBehaviour
+    {
+        [SerializeField] [AutoProperty] private EnemyHealth _health;
+        [SerializeField] [AutoProperty] private Animator _animator;
+        [SerializeField] [AutoProperty] private Rigidbody[] _rigidbodies;
+
+        private void Start() => 
+            _rigidbodies.ForEach(r => r.isKinematic = true);
+
+        private void OnEnable() =>
+            _health.Dead += EnableRagdoll;
+        
+        private void OnDisable() =>
+            _health.Dead -= EnableRagdoll;
+
+        private void EnableRagdoll(EnemyHealth _)
+        {
+            _animator.enabled = false;
+            _rigidbodies.ForEach(r => r.isKinematic = false);
+        }
+    }
+}
