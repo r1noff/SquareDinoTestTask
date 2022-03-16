2c7153f9e24fc8d46c957b45b72e9142964ea7aa
Ivan Rechkin <rechkinin@gmail.com>
2022-03-16 07:55:39 +0500

    Health Indicator

--- Assets/_Project/Code/Enemies/BattleZone.cs
+++ Assets/_Project/Code/Enemies/BattleZone.cs
@@ -28,11 +28,13 @@ namespace SquareDino.RechkinTestTask.Enemies
         private void SpawnEnemyBulk()
         {
             int linesCount = Mathf.CeilToInt(1f * _enemiesCount / LineCount);
+            var spawnedCount = 0;
             for (var i = 0; i < linesCount; i++)
             {
                 for (var j = 0; j < LineCount; j++)
                 {
-                    if (_currentEnemiesCount >= _enemiesCount) return;
+                    if (spawnedCount >= _enemiesCount) return;
+                    spawnedCount++;
                     Vector3 delta = _spawnPoint.position + new Vector3(j * Spacing, 0, i * Spacing);
                     SpawnEnemy(delta);
                 }
---
+++ Assets/_Project/Code/Enemies/HealthIndicator.cs
@@ -0,0 +1,27 @@
+﻿using MyBox;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SquareDino.RechkinTestTask.Enemies
+{
+    public class HealthIndicator : MonoBehaviour
+    {
+        [SerializeField] [AutoProperty] private Slider _slider;
+        [SerializeField] [AutoProperty] private EnemyHealth _health;
+
+        private void Start()
+        {
+            _slider.maxValue = _health.MaxValue;
+            _slider.value = _health.Value;
+        }
+
+        private void OnEnable() =>
+            _health.ValueChanged += OnHealthChanged;
+
+        private void OnDisable() =>
+            _health.ValueChanged -= OnHealthChanged;
+        
+        private void OnHealthChanged() => 
+            _slider.value = _health.Value;
+    }
+}
